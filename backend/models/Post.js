3dad44e0809ce97297bdd1018794948460b3804d
peterme12345 <npeter389@gmail.com>
2023-10-14 13:48:13 -0700

    added post bar in home page and routed to home

--- backend/models/Post.js
+++ backend/models/Post.js
@@ -20,4 +20,4 @@ const postSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-module.exports = mongoose.model("Post", postSchema);
+module.exports = postSchema;
--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Login from "./Login";
 import Register from "./Register";
+import Home from "./pages/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/Register" element={<Register />}></Route>
         <Route path="/Login" element={<Login />}></Route>
+        <Route path="/" element={<Home />}></Route>
       </Routes>
     </BrowserRouter>
   );
