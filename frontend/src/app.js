2cc595f35c04f9e93bf62b4131e252d4564463d1
peterme12345 <npeter389@gmail.com>
2023-10-26 20:31:30 -0700

    Revert "Merge pull request #9 from peterme12345/cleaningupfiles"
    This reverts commit ccc407b76a2fd6ef5cbb21b0114d8acf33d952e1, reversing
    changes made to 8e0d4e13c0230e127b7f36d170da6a8ff06956bc.

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
+import Login from "./Login";
+import Register from "./Register";
 import Home from "./pages/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
