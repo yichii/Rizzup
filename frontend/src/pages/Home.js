fadfe0649f0721169c61dd5ec39ee047df5d5897
yichiii <55825777+yichii@users.noreply.github.com>
2023-11-19 21:26:19 -0800

    Remove Unnecessary Components
    Remove components such as topics and images to remove clutter

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { Navbar } from "../components/Navbar";
-import Topics from "../components/Topics";
 import axios from "axios";
 
 const HomePage = () => {
@@ -75,29 +74,6 @@ const HomePage = () => {
                 <p>Author: {post.author}</p>
               </div>
             ))}
-          </div>
-
-          <div className="row">
-            <div className="col-sm-7">
-              <Topics />
-            </div>
-            <div className="col-sm-5">
-              <img
-                src="https://mondrian.mashable.com/uploads%252F2018%252F11%252F15%252FCRA_01863.jpg%252Ffit-in__1440x1440.jpg?signature=Xo5f47jqQPosE6LEgOZaQGqiOng="
-                alt=""
-                style={{ width: "100%", height: "auto" }}
-              />
-              <img
-                src="https://hips.hearstapps.com/hmg-prod/images/cra-04890-1533750260.jpg"
-                alt=""
-                style={{ width: "100%", height: "auto" }}
-              />
-              <img
-                src="https://assets3.thrillist.com/v1/image/2779700/1200x630/flatten;crop_down;webp=auto;jpeg_quality=70"
-                alt=""
-                style={{ width: "100%", height: "auto" }}
-              />
-            </div>
           </div>
         </div>
       </div>
