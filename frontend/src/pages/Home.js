fe124536f5016e8b3566ff0161c83c1fa49b9b3b
yichiii <55825777+yichii@users.noreply.github.com>
2023-12-12 14:42:17 -0800

    have posts look like cards

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -28,6 +28,7 @@ const HomePage = () => {
 
       if (response.status === 201) {
         setPost("");
+        setTitle("");
       }
     } catch (error) {
       console.error("Error creating a post:", error);
@@ -107,14 +108,7 @@ const HomePage = () => {
     <div>
       <Navbar />
       <div className="container">
-        <h2
-          className="my-4"
-          style={{ color: "var(--bs-ternary)", fontSize: "30px" }}
-        >
-          Topics
-        </h2>
         <div className="form-container">
-          <h1>Post</h1>
           <form onSubmit={handlePostSubmit}>
             <div className="form-group">
               <input
@@ -138,10 +132,17 @@ const HomePage = () => {
           </form>
           <div>
             {posts.map((post) => (
-              <div key={post._id}>
+              <div className="card" key={post._id}>
+                <div className="author">
+                  <p>Profile Picture</p>
+                  <div>
+                    <p>{post.author.username}</p>
+                    <p>Time posted since</p>
+                  </div>
+                </div>
+                
                 <h3>{post.title}</h3>
-                <p>Content: {post.content}</p>
-                <p>Author: {post.author.username}</p>
+                <p>{post.content}</p>
                 <form onSubmit={(e) => handleCommentSubmit(e, post._id)}>
                   <div className="form-group">
                     <input
@@ -166,25 +167,8 @@ const HomePage = () => {
 
           <div className="row">
             <div className="col-sm-7">
-              <Topics />
             </div>
-            <div className="col-sm-5">
-              <img
-                src="https://mondrian.mashable.com/uploads%252F2018%252F11%252F15%252FCRA_01863.jpg%252Ffit-in__1440x1440.jpg?signature=Xo5f47jqQPosE6LEgOZaQGqiOng="
-                alt=""
-                style={{ width: "100%", height: "auto" }}
-              />
-              <img
-                src="https://hips.hearstapps.com/hmg-prod/images/cra-04890-1533750260.jpg"
-                alt=""
-                style={{ width: "100%", height: "auto" }}
-              />
-              <img
-                src="https://assets3.thrillist.com/v1/image/2779700/1200x630/flatten;crop_down;webp=auto;jpeg_quality=70"
-                alt=""
-                style={{ width: "100%", height: "auto" }}
-              />
-            </div>
+            
           </div>
         </div>
       </div>
