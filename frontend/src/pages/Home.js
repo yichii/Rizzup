2c1d6dd9d869a392d9268c2ef17aa2889d8ff4ed
peterme12345 <npeter389@gmail.com>
2023-12-12 17:13:56 -0800

    made home page look cleaner

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Navbar } from "../components/Navbar";
-import Topics from "../components/Topics";
 import axios from "axios";
+import "./home.css";
 
 const HomePage = () => {
   // const [comments, setComments] = useState({});
@@ -110,7 +110,7 @@ const HomePage = () => {
           className="my-4"
           style={{ color: "var(--bs-ternary)", fontSize: "30px" }}
         >
-          Topics
+          Feed
         </h2>
         <div className="form-container">
           <h1>Post</h1>
@@ -129,7 +129,7 @@ const HomePage = () => {
           </form>
           <div>
             {posts.map((post) => (
-              <div key={post._id}>
+              <div key={post._id} className="post-container">
                 <h3>{post.title}</h3>
                 <p>Content: {post.content}</p>
                 <p>Author: {post.author.username}</p>
@@ -156,26 +156,7 @@ const HomePage = () => {
           </div>
 
           <div className="row">
-            <div className="col-sm-7">
-              <Topics />
-            </div>
-            <div className="col-sm-5">
-              <img
-                src="https://mondrian.mashable.com/uploads%252F2018%252F11%252F15%252FCRA_01863.jpg%252Ffit-in__1440x1440.jpg?signature=Xo5f47jqQPosE6LEgOZaQGqiOng="
-                alt=""
-                style={{ width: "100%", height: "auto" }}
-              />
-              <img
-                src="https://hips.hearstapps.com/hmg-prod/images/cra-04890-1533750260.jpg"
-                alt=""
-                style={{ width: "100%", height: "auto" }}
-              />
-              <img
-                src="https://assets3.thrillist.com/v1/image/2779700/1200x630/flatten;crop_down;webp=auto;jpeg_quality=70"
-                alt=""
-                style={{ width: "100%", height: "auto" }}
-              />
-            </div>
+            <div className="col-sm-7"></div>
           </div>
         </div>
       </div>
